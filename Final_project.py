4d88e8c86d6f1e998e1fb5de9aab3d7655c9ca8a
Lirov <40965021+Lirov@users.noreply.github.com>
2018-12-24 19:50:44 +0200

    Update Final_project.py

--- Final_project.py
+++ Final_project.py
@@ -1,6 +1,6 @@
 from selenium import webdriver
 driver = webdriver.Chrome(executable_path="C:\selenium\chromeDriver_selenium\chromedriver.exe")
 driver.get("http://192.168.99.100:5000")
+driver.find_element_by_xpath("//html/body");
 print()
-
 
